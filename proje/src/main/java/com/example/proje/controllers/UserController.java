6d44fc0485d49ddb88f441341ebab1f12a4ea8bf
arif <arif.cettin@gmail.com>
2023-10-10 17:13:07 +0300

    UserDto configuration

--- proje/src/main/java/com/example/proje/controllers/UserController.java
+++ proje/src/main/java/com/example/proje/controllers/UserController.java
@@ -1,38 +1,41 @@
 package com.example.proje.controllers;
 
 
+import com.example.proje.dto.UserDto;
 import com.example.proje.entities.User;
 import com.example.proje.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
+    private final ModelMapper modelMapper;
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ModelMapper modelMapper) {
         this.userService = userService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public User createUser(@RequestBody User newUser){
-        return userService.createUser(newUser);
+    public User createUser(@RequestBody UserDto userDto){
+        return userService.createUser(modelMapper.map(userDto,User.class));
     }
 
 
     @GetMapping
-    public List<User> getUsers(){
+    public List<UserDto> getUsers(){
         return userService.getUsers();
     }
 
     @GetMapping("/{userId}")
-    public User getOneUser(@PathVariable Long userId){
+    public UserDto getOneUser(@PathVariable Long userId){
         return userService.getOneUser(userId);
     }
 
@@ -42,7 +45,7 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public User userUpdate(@RequestBody User updateUser, @PathVariable Long userId){
-        return userService.updateUser(updateUser,userId);
+    public UserDto userUpdate(@RequestBody UserDto userDto, @PathVariable Long userId){
+        return userService.updateUser(userDto,userId);
     }
 }
--- proje/src/main/java/com/example/proje/services/UserService.java
+++ proje/src/main/java/com/example/proje/services/UserService.java
@@ -1,50 +1,59 @@
 package com.example.proje.services;
 
 
+import com.example.proje.config.SecurityConfig;
+import com.example.proje.dto.UserDto;
 import com.example.proje.entities.User;
 import com.example.proje.repo.UserRepository;
+import com.example.proje.security.JwtTokenProvider;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final SecurityConfig securityConfig;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ModelMapper modelMapper, SecurityConfig securityConfig) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+        this.securityConfig = securityConfig;
     }
 
     public User createUser(User newUser) {
         return userRepository.save(newUser);
     }
 
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getUsers() {
+        return userRepository.findAll().stream().map(user -> modelMapper.map(user, UserDto.class)).collect(Collectors.toList());
     }
 
     public void userDelete(Long userId) {
         userRepository.deleteById(userId);
     }
 
-    public User updateUser(User updateUser, Long userId) {
+    public UserDto updateUser(UserDto userDto, Long userId) {
         Optional<User> user = userRepository.findById(userId);
         if(!user.isPresent()){
             return null;
         }
-        user.get().setUserName(updateUser.getUserName());
-        user.get().setPassword(updateUser.getPassword());
+        user.get().setUserName(userDto.getUserName());
+        user.get().setPassword(securityConfig.passwordEncoder().encode(userDto.getPassword()));
         userRepository.save(user.get());
-        return user.get();
+        return modelMapper.map(user.get(), UserDto.class);
     }
 
 
-    public User getOneUser(Long userId) {
-        return userRepository.findById(userId).orElse(null);
+    public UserDto getOneUser(Long userId) {
+        return modelMapper.map(userRepository.findById(userId),UserDto.class);
     }
 
     public User getOneUserByUserName(String userName) {
